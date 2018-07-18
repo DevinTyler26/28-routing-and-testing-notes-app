5a6ff3be397a0ca7795506dd2b8ea8577b1cb299
Devin Cunningham <dtyler.cunningham@gmail.com>
2018-07-18 12:55:35 -0700

    empty tests

---
+++ src/__test__/dashboard.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Dashboard from '../components/dashboard/dashboard';
+
+configure({ adapter: new Adapter() });
+
+describe('Dashboard testing', () => {
+  let mountedDashboard;
+  beforeEach(() => {
+    mountedDashboard = mount(<Dashboard />);
+  });
+  afterEach(() => {
+    mountedDashboard.unmount();
+  });
+  test('Simple test for initail state', () => {
+
+  });
+  test('Adding a new note to the state', () => {
+
+  });
+});
